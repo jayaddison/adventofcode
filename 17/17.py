dc5e40133d959d0cfcb7a4ed7f9071c111d8ac1e
James Addison <jay@jp-hosting.net>
2021-12-17 19:12:06 +0000

    Part one: preparation

---
+++ 17/17.py
@@ -0,0 +1,50 @@
+class Probe:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    def step(self):
+        self.x += self.velocity_x
+        self.y += self.velocity_y
+        self.velocity_x += (1 if self.velocity_x < 0 else -1)
+        self.velocity_y -= 1
+
+
+class Target:
+
+    def _parse_range(self, text):
+        axis, range = text.split("=")
+        min_val, max_val = range.split("..")
+        return int(min_val), int(max_val)
+
+    def _parse_coordinates(self, text):
+        preamble, coordinates = text.split(":")
+        xrange, yrange = coordinates.strip().split(",")
+        xmin, xmax = self._parse_range(xrange.strip())
+        ymin, ymax = self._parse_range(yrange.strip())
+        return xmin, xmax, ymin, ymax
+
+    def __init__(self, text):
+        xmin, xmax, ymin, ymax = self._parse_coordinates(text)
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+        
+    def contains(self, probe):
+        within_x = self.xmin <= probe.x <= self.xmax
+        within_y = self.ymin <= probe.y <= self.ymax
+        return within_x and within_y
+
+# Tests
+p = Probe(x=25, y=-7)
+t = Target("target area: x=20..30, y=-10..-5")
+
+assert t.contains(p)
+
+# Program
+content = open("17.txt").read().strip()
+
+p = Probe()
+t = Target(content)
