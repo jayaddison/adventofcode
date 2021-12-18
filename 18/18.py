05335ecc85eb798ba288764f594a3a5a5ef01d5b
James Addison <jay@jp-hosting.net>
2021-12-18 18:14:55 +0000

    Get test coverage passing for single-iteration explode cases

--- 18/18.py
+++ 18/18.py
@@ -13,33 +13,32 @@ class Snailfish:
         total.reduce()
         return total
 
-    def add_rhs(self, value, limit):
-        target = self.right if self.right != limit else self.left
-        if isinstance(target, Snailfish):
-            target.add_rhs(value, limit)
-        else:
-            target += value
-
     def reduce(self):
         stack = [(self, 1)]
         current_node = None
         previous_node = None
 
-        previous_number = 0
+        modified = False
+        previous_number_node = None
         carry_number = 0
 
         while stack:
             current_node, depth = stack.pop()
+
+            if not isinstance(current_node.left, Snailfish):
+                previous_number_node = current_node
+
             if carry_number and not isinstance(current_node.left, Snailfish):
                 current_node.left += carry_number
                 carry_number = 0
 
-            if depth == 4:
+            if depth == 4 and not modified:
                 explode = None
                 if isinstance(current_node.left, Snailfish):
                     explode = current_node.left
                 elif isinstance(current_node.right, Snailfish):
                     explode = current_node.right
+                modified = explode is not None
 
                 if current_node.left == explode:
                     current_node.left = 0
@@ -47,15 +46,19 @@ class Snailfish:
 
                 elif current_node.right == explode:
                     current_node.right = 0
-                    current_node.left += explode.left
+                    previous_number_node.left += explode.left
                     carry_number = explode.right
 
-            if isinstance(current_node.left, Snailfish):
-                stack.append((current_node.left, depth + 1))
             if isinstance(current_node.right, Snailfish):
                 stack.append((current_node.right, depth + 1))
+            if isinstance(current_node.left, Snailfish):
+                stack.append((current_node.left, depth + 1))
 
             previous_node = current_node
+
+        if carry_number and not isinstance(self.right, Snailfish):
+            self.right += carry_number
+            carry_number = 0
 
     def __str__(self):
         return f"[{self.left},{self.right}]"
@@ -101,6 +104,9 @@ class SnailfishParser(NodeVisitor):
 for test_input, expected_output in [
     ("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
     ("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+    ("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"),
+    ("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+    ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
 ]:
     test_snailfish = SnailfishParser(test_input).process()
     test_snailfish.reduce()
@@ -108,6 +114,7 @@ for test_input, expected_output in [
     print(f"in: {test_input}")
     print(f"ac: {test_snailfish}")
     print(f"ex: {expected_output}")
+    print()
 
     assert str(test_snailfish) == expected_output
 
