d3ace238bd73b298f147c14b318f7d4e19b207bc
James Addison <jay@jp-hosting.net>
2021-12-18 17:40:30 +0000

    Hacky, imcomplete implementation for day eighteen (part one)

--- 18/18.py
+++ 18/18.py
@@ -13,8 +13,49 @@ class Snailfish:
         total.reduce()
         return total
 
+    def add_rhs(self, value, limit):
+        target = self.right if self.right != limit else self.left
+        if isinstance(target, Snailfish):
+            target.add_rhs(value, limit)
+        else:
+            target += value
+
     def reduce(self):
-        pass
+        stack = [(self, 1)]
+        current_node = None
+        previous_node = None
+
+        previous_number = 0
+        carry_number = 0
+
+        while stack:
+            current_node, depth = stack.pop()
+            if carry_number and not isinstance(current_node.left, Snailfish):
+                current_node.left += carry_number
+                carry_number = 0
+
+            if depth == 4:
+                explode = None
+                if isinstance(current_node.left, Snailfish):
+                    explode = current_node.left
+                elif isinstance(current_node.right, Snailfish):
+                    explode = current_node.right
+
+                if current_node.left == explode:
+                    current_node.left = 0
+                    current_node.right = explode.left
+
+                elif current_node.right == explode:
+                    current_node.right = 0
+                    current_node.left += explode.left
+                    carry_number = explode.right
+
+            if isinstance(current_node.left, Snailfish):
+                stack.append((current_node.left, depth + 1))
+            if isinstance(current_node.right, Snailfish):
+                stack.append((current_node.right, depth + 1))
+
+            previous_node = current_node
 
     def __str__(self):
         return f"[{self.left},{self.right}]"
@@ -55,6 +96,20 @@ class SnailfishParser(NodeVisitor):
     def process(self):
         return self.visit(self.tree)
 
+
+# Test cases
+for test_input, expected_output in [
+    ("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
+    ("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+]:
+    test_snailfish = SnailfishParser(test_input).process()
+    test_snailfish.reduce()
+
+    print(f"in: {test_input}")
+    print(f"ac: {test_snailfish}")
+    print(f"ex: {expected_output}")
+
+    assert str(test_snailfish) == expected_output
 
 content = open("18.txt").read().strip()
 
