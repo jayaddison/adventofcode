3371611ee22b39668a44cd65436b16c6dd5e9d55
James Addison <jay@jp-hosting.net>
2021-12-20 11:14:31 +0000

    Near-complete solution for day twenty (part one)

--- 20/20.py
+++ 20/20.py
@@ -1,29 +1,101 @@
 content = open("20.txt").read()
 
-input_algorithm, input_image = content.split("\n\n")
+input_algorithm, input_image_text = content.split("\n\n")
+input_algorithm = [True if char == "#" else False for char in input_algorithm]
 
-algorithm = ['1' if char == '#' else 0 for char in input_algorithm]
-
-image = []
-for line in input_image.split("\n"):
+input_image = []
+for line in input_image_text.split("\n"):
     line = line.strip()
     if not line:
         continue
-    row = ['1' if char == '#' else '0' for char in line]
-    image.append(row)
+    row = [char == "#" for char in line]
+    input_image.append(row)
 
-def input_pixel_to_bitstring(x, y):
+
+def pixel_neighbour_bitstring(source, x, y):
     bitstring = []
     for row in [y - 1, y, y + 1]:
         for col in [x - 1, x, x + 1]:
-            bitstring.append(image[row][col])
+            try:
+                bit = "1" if source[row][col] else "0"
+            except:
+                bit = "0"
+            bitstring.append(bit)
     return str().join(bitstring)
 
-def output_pixel(x, y):
-    index_bitstring = input_pixel_to_bitstring(x, y)
+
+def transform_pixel(source, x, y, algorithm):
+    index_bitstring = pixel_neighbour_bitstring(source, x, y)
     index = int(index_bitstring, 2)
     return algorithm[index]
-    
 
-print(len(algorithm))
-print(output_pixel(2, 2))
+
+def render(source):
+    width = len(source[0])
+    height = len(source)
+    for y in range(height):
+        for x in range(width):
+            print("#" if source[y][x] else ".", end="")
+        print()
+    print(count_light_pixels(source))
+    print()
+
+
+def empty_canvas(source):
+    width = len(source[0])
+    height = len(source)
+
+    result = []
+    for _ in range(height):
+        result.append([False] * width)
+    return result
+
+
+def has_light_border(source):
+    for y in (0, len(source) - 1):
+        for value in source[y]:
+            if value:
+                return True
+    for x in (0, len(source[0]) - 1):
+        for y in range(len(source)):
+            if source[y][x]:
+                return True
+    return False
+
+
+def expand(source):
+    width = len(source[0])
+    height = len(source)
+
+    result = []
+    result.append([False] * (width + 2))
+    for y in range(height):
+        result.append([False] + source[y] + [False])
+    result.append([False] * (width + 2))
+    return result
+
+
+def apply_algorithm(source, algorithm):
+    destination = empty_canvas(source)
+    width = len(destination[0])
+    height = len(destination)
+    for x in range(width):
+        for y in range(height):
+            destination[y][x] |= transform_pixel(source, x, y, algorithm)
+    return destination
+
+
+def apply_algorithm_repeatedly(source, algorithm, steps=1):
+    for _ in range(steps):
+        if has_light_border(source):
+            source = expand(source)
+        source = apply_algorithm(source, algorithm)
+        render(source)
+    return source
+
+
+def count_light_pixels(source):
+    return sum(sum(row) for row in source)
+
+
+apply_algorithm_repeatedly(input_image, input_algorithm, steps=2)
