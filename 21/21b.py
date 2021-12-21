2138ed5c14daa3f2c2d071159bf36da0b9e9f83d
James Addison <jay@jp-hosting.net>
2021-12-21 21:17:14 +0000

    Code for day twenty-one (part two)

--- 21/21b.py
+++ 21/21b.py
@@ -1,12 +1,5 @@
-from abc import ABC, abstractmethod
-from copy import deepcopy
+from collections import defaultdict
 from itertools import product
-
-
-class Die(ABC):
-    @abstractmethod
-    def roll(self):
-        pass
 
 
 class Player:
@@ -18,12 +11,17 @@ class Player:
     def __str__(self):
         return self.name
 
-    def play(self, a, b, c):
-        advance = sum([a, b, c])
+    def play(self, advance):
         self.pos = self.pos + advance
         while self.pos > 10:
             self.pos -= 10
         self.score += self.pos
+
+    def play_path(self, path):
+        for step in path:
+            self.play(step)
+            if self.has_won:
+                return self
 
     @property
     def has_won(self):
@@ -40,20 +38,49 @@ for line in content.split("\n"):
     preamble, pos = line.split(":")
     player_name, _ = preamble.split("starting position")
     players.append(Player(player_name, int(pos)))
-winning_paths = {player.name: [] for player in players}
+games_won = defaultdict(lambda: 0)
 
 
-def explore_paths(player, path):
-    faces = [1, 2, 3]
-    for a, b, c in product(faces, faces, faces):
-        player_copy = deepcopy(player)
-        player_copy.play(a, b, c)
-        if player_copy.has_won:
-            winning_path = path + [a, b, c]
-            winning_paths[player.name].append(winning_path)
-            print(f"{winning_path} is a winning path for {player}")
+faces = [1, 2, 3]
+outcome_counts = defaultdict(lambda: 0)
+for a, b, c in product(faces, repeat=3):
+    outcome = sum([a, b, c])
+    outcome_counts[outcome] += 1
+total_outcomes = sum(outcome_counts.values())
+outcome_frequencies = {
+    outcome: outcome_count / total_outcomes
+    for outcome, outcome_count in outcome_counts.items()
+}
+
+
+def path_to_occurrences(path):
+    outcome_frequency = 1.0
+    for roll in path:
+        roll_frequency = outcome_frequencies[roll]
+        outcome_frequency *= roll_frequency
+    possible_games = pow(3, len(path) * 3)
+    return round(possible_games * outcome_frequency)
+
+
+def explore_paths(players, path):
+    for outcome in outcome_frequencies:
+        evaluation_path = path + [outcome]
+        winner = None
+        for index, player in enumerate(players):
+            simulated_player = Player(name=player.name, pos=player.pos)
+            player_rolls = evaluation_path[index :: len(players)]
+            winner = simulated_player.play_path(player_rolls)
+            if winner:
+                break
+        if winner:
+            games_won[player] += path_to_occurrences(evaluation_path)
         else:
-            explore_paths(player_copy, path + [a, b, c])
+            explore_paths(players, evaluation_path)
 
-for player in players:
-    explore_paths(player, [])
+
+assert path_to_occurrences([3]) == 1
+assert path_to_occurrences([6, 3]) == 7
+assert path_to_occurrences([3, 6]) == 7
+
+explore_paths(players, [])
+print(games_won)
