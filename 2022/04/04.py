f69ea07a8d4459e0381f22b59f4fa53f520b58e7
James Addison <jay@jp-hosting.net>
2022-12-04 21:30:21 +0000

    Add initial code and sample data for 2022-12-04, part one

---
+++ 2022/04/04.py
@@ -0,0 +1,15 @@
+content = open("04.txt").read()
+
+def sections(assignment):
+    first_section, last_section = assignment.split("-")
+    return set(section for section in range(int(first_section), int(last_section) + 1))
+
+fully_contained_count = 0
+for line in content.splitlines():
+    assignment_one, assignment_two = line.split(",")
+    sections_one, sections_two = sections(assignment_one), sections(assignment_two)
+
+    fully_contained_count += sections_one < sections_two
+    fully_contained_count += sections_two < sections_one
+
+print(fully_contained_count)
