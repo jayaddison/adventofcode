c133b105b64ffb6a91c1a8450138d259ed165024
James Addison <jay@jp-hosting.net>
2022-12-04 21:35:32 +0000

    Fixup: solution for 2022-12-04, part one

--- 2022/04/04.py
+++ 2022/04/04.py
@@ -8,8 +8,6 @@ fully_contained_count = 0
 for line in content.splitlines():
     assignment_one, assignment_two = line.split(",")
     sections_one, sections_two = sections(assignment_one), sections(assignment_two)
-
-    fully_contained_count += sections_one < sections_two
-    fully_contained_count += sections_two < sections_one
+    fully_contained_count += (sections_one <= sections_two or sections_two <= sections_one)
 
 print(fully_contained_count)
