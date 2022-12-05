e90cc56a3869acf9b0b65117b63c5c81ecf84b69
James Addison <jay@jp-hosting.net>
2022-12-05 19:49:03 +0000

    Add initial code and sample data for 2022-12-05, part one

---
+++ 2022/05/05.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+content = open("05.txt").read()
+
+supplies, operations = content.split("\n\n")
+
+
+def parse_stacks(supplies):
+    stacks = defaultdict(list)
+    for line in supplies.splitlines():
+        indices = {idx: char for idx, char in enumerate(line) if char.isupper()}
+        for index, crate in indices.items():
+           stacks[index].insert(0, crate)
+        if not indices:
+            numbers = {idx: char for idx, char in enumerate(line) if char.isdigit()}
+            for index, number in numbers.items():
+                stacks[number] = stacks.pop(index, [])
+    return stacks
+
+def move(from_stack, to_stack, item_count):
+    for _ in range(item_count):
+        to_stack.append(from_stack.pop())
+
+stacks = parse_stacks(supplies)
+for line in operations.splitlines():
+    operation, _, arguments = line.partition(" from ")
+    op_name, _, op_count = operation.partition(" ")
+    from_stack, _, to_stack = arguments.partition(" to ")
+    assert op_name == "move"
+    move(stacks[from_stack], stacks[to_stack], int(op_count))
+
+print("".join(stack.pop() if stack else "" for stack in stacks.values()))
