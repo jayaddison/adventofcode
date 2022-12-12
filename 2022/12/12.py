857e0036babd3118f446b291edaccd7c00817c6a
James Addison <jay@jp-hosting.net>
2022-12-12 23:02:36 +0000

    Try again, this time re-using code from a similar exploration puzzle from last year (2021-12-15)
    Note: not quite working yet, though.  It seems likely-to-probable that the 'neighbor' method-overload isn't implemented correctly -- or that there's a flaw with the x/y co-ordinate mappings.

--- 2022/12/12.py
+++ 2022/12/12.py
@@ -1,32 +1,3 @@
-class Tile:
-
-    def __init__(self, elevation):
-        self.elevation = elevation
-
-    def calculate_reachable_tiles(self, surroundings):
-        self.reachable_tiles = [
-            step for step in surroundings
-            if abs(step.elevation - self.elevation) <= 1
-        ]
-
-    def explore(self, path, destination):
-        # Base case: we've reached the destination; return the entire path
-        if self == destination:
-            return [list(path) + [self]]
-
-        # Avoid exploring the same tile repeatedly
-        if self in path:
-            return []
-
-        # Recursive case: explore paths for neighbouring reachable tiles
-        paths = []
-        for tile in self.reachable_tiles:
-            for result in tile.explore(path=path | {self}, destination=destination):
-                if result:
-                    paths.append(result)
-        return paths
-
-
 def line_to_elevations(line):
     start, end, elevations = None, None, []
     for j, char in enumerate(line):
@@ -41,31 +12,39 @@ grid = []
 content = open("12.txt").read()
 for i, line in enumerate(content.splitlines()):
     start, end, elevations = line_to_elevations(line)
-    grid.append([Tile(elevation) for elevation in elevations])
+    grid.append([elevation for elevation in elevations])
     if start is not None:
         start_position = (i, start)
     if end is not None:
         end_position = (i, end)
 
 
-# Connect each map tile to each of its reachable neighbours (creating a graph)
-for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        surroundings = []
-        for step_y, step_x in (0, 1), (0, -1), (1, 0), (-1, 0):
-            try:
-                surroundings.append(grid[i + step_y][j + step_x])
-            except IndexError:
-                pass
-        tile = grid[i][j]
-        tile.calculate_reachable_tiles(surroundings)
-
-
 # Explore the graph
 (start_y, start_x), (end_y, end_x) = start_position, end_position
-start, end = grid[start_y][start_x], grid[end_y][end_x]
-paths = start.explore(path=set(), destination=end)
-print(min([len(path) for path in paths]))
+
+from pathfinding.core.grid import Grid
+from pathfinding.finder.dijkstra import AStarFinder
+from pathfinding.core.diagonal_movement import DiagonalMovement
+
+class TileGrid(Grid):
+    def neighbors(self, node, diagonal_movement=DiagonalMovement.never):
+        neighbours = super().neighbors(node, diagonal_movement)
+        elevation = grid[node.y][node.x]
+        for neighbour in neighbours:
+            if abs(grid[neighbour.y][neighbour.x] - elevation) <= 1:
+                yield neighbour
+
+    def walkable(self, x, y):
+        return super().inside(x, y)
+
+
+tile_grid = TileGrid(matrix=grid)
+origin = tile_grid.node(start_x, start_y)
+destination = tile_grid.node(end_x, end_y)
+finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+path, _ = finder.find_path(origin, destination, tile_grid)
+print(path)
+print(len(path))
 
 
 assert (1, 5, [0, 0, 2, 3, 4, 25, 25]) == line_to_elevations("aScdeEz")
