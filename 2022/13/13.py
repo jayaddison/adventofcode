59eea525dd50342d46f40ed9e14dbdb986609a37
James Addison <jay@jp-hosting.net>
2022-12-13 21:37:44 +0000

    Re-use code from a similar string-format-parsing subproblem from last year (2021-12-18)

--- 2022/13/13.py
+++ 2022/13/13.py
@@ -1,3 +1,57 @@
+from parsimonious.grammar import Grammar
+from parsimonious.nodes import NodeVisitor
+
+
+class NestedIntegerListParser(NodeVisitor):
+
+    grammar = Grammar(
+        """
+        integer_list = open_bracket left comma right close_bracket
+
+        left = number
+             / integer_list
+
+        right = number
+              / integer_list
+
+        open_bracket = "["
+        close_bracket = "]"
+        comma = ","
+        number = ~"[0-9]+"
+        """
+    )
+
+    def __init__(self, text):
+        self.tree = self.grammar.parse(text)
+
+    def visit_integer_list(self, node, visited_children):
+        _, left, _, right, _ = visited_children
+        return left + right
+
+    def visit_number(self, node, visited_children):
+        return int(node.text)
+
+    def generic_visit(self, node, visited_children):
+        return visited_children
+
+    def process(self):
+        return self.visit(self.tree)
+
+
+# Test parsing
+for test_input, expected_output in [
+    ("[[[[[9,8],1],2],3],4]", [[[[[9,8],1],2],3],4]),
+    ("[7,[6,[5,[4,[3,2]]]]]", [7,[6,[5,[4,[3,2]]]]]),
+    ("[[6,[5,[4,[3,2]]]],1]", [[6,[5,[4,[3,2]]]],1]),
+    ("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]),
+    ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]),
+    ("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]),
+    ("[[[[14,13],[8,15]],[[12,5],[10,0]]],[[[11,[7,4]],7],1]]", [[[[14,13],[8,15]],[[12,5],[10,0]]],[[[11,[7,4]],7],1]]),
+]:
+    test_nested_integer_list = NestedIntegerListParser(test_input).process()
+    assert test_nested_integer_list == expected_output
+
+
 def compare(left, right):
     match type(left).__name__, type(right).__name__:
 
