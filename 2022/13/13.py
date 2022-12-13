d86b57e494861d9f868314582b97818efa772fc7
James Addison <jay@jp-hosting.net>
2022-12-13 22:53:23 +0000

    Expand test coverage

--- 2022/13/13.py
+++ 2022/13/13.py
@@ -107,3 +107,4 @@ assert compare([1], [2]) == True  # lower integer first
 assert compare([1, 2], [2]) == True  # left items exhausted; all lower than corresponding right items
 assert compare([3, 2], [1]) == False
 assert compare([1], [2, 3]) == True
+assert compare([1, 2, 3, 4, 5], [1, 2, 5, 4, 5]) == True
