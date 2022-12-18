846719169ee31e834f3941ae9eaba8773c6593cf
James Addison <jay@jp-hosting.net>
2022-12-18 22:31:07 +0000

    Update code to handle single/plural phrasing

--- 2022/16/16.py
+++ 2022/16/16.py
@@ -4,7 +4,8 @@ def parse_line(line):
     definition_segment, link_segment = line.split(";", 1)
     name, rate_segment = definition_segment[6:].split(" ", 1)
     rate = int(rate_segment[14:])
-    links = link_segment[24:].split(", ")
+    link_words = link_segment.split(" ")
+    links = "".join(link_words[5:]).split(",")
     return name, rate, links
 
 
@@ -12,4 +13,10 @@ assert parse_line("Valve FF has flow rate=0; tunnels lead to valves EE, GG") == 
     "FF",
     0,
     ["EE", "GG"],
+)
+
+assert parse_line("Valve TT has flow rate=99; tunnel leads to valve TT") == (
+    "TT",
+    99,
+    ["TT"],
 )
