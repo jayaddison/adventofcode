6f8863a7bbae32def78e495678458a5606756190
James Addison <jay@jp-hosting.net>
2023-12-10 22:11:26 +0000

    2023-12-04: part two: solution code

--- 2023/04/04.py
+++ 2023/04/04.py
@@ -1,12 +1,20 @@
 content = open("04.txt").read()
 
-score = 0
+cards, card_tallies = {}, {}
 for line in content.splitlines():
-    _, _, numbers = line.partition(": ")
+    card, _, numbers = line.partition(": ")
+    card_id = int(card.replace("Card ", ""))
     winning_numbers, _, found_numbers = numbers.partition(" | ")
     winning_numbers = set(filter(None, winning_numbers.split(" ")))
     found_numbers = set(filter(None, found_numbers.split(" ")))
+    cards[card_id] = (winning_numbers, found_numbers)
+    card_tallies[card_id] = 1
+
+for card_id, card in cards.items():
+    winning_numbers, found_numbers = card
     matched = len(winning_numbers & found_numbers)
     if matched:
-        score += 2 ** (matched - 1)
-print(score)
+        for winning_card_id in range(card_id + 1, card_id + matched + 1):
+            card_tallies[winning_card_id] += card_tallies[card_id]
+
+print(sum(card_tallies.values()))
