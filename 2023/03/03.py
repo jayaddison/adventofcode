55ad7b20cdc3ee5d2748e61223ed6ef1388c3079
James Addison <jay@jp-hosting.net>
2023-12-10 21:35:48 +0000

    2023-12-03: part two: cleanup and comment updates

--- 2023/03/03.py
+++ 2023/03/03.py
@@ -3,7 +3,7 @@ content = open("03.txt").read()
 part_grid = {}
 symbol_grid = {}
 
-# Find potential part numbers and symbols in the grid
+# Find potential part numbers and gear symbols in the grid
 for y, line in enumerate(content.splitlines()):
     part_number = ""
     part_grid[y] = {}
@@ -21,21 +21,17 @@ for y, line in enumerate(content.splitlines()):
         part_grid[y][x - len(part_number)] = part_number
         part_number = ""
 
-# Filter the part numbers to cases where a symbol is adjacent
-filtered_grid = {}
+# Record part numbers adjacent to gear symbols
 for y, part_map in part_grid.items():
     for x, part_number in part_map.items():
         for yoffset in (-1, 0, 1):
             for xoffset in range(-1, len(part_number) + 1):
                 try:
                     symbol_grid[y + yoffset][x + xoffset].append(int(part_number))
-                    if y not in filtered_grid:
-                        filtered_grid[y] = {}
-                    filtered_grid[y][x] = part_number
                 except:
                     pass
 
-# Sum the filtered part numbers
+# Sum the multiplied-part-numbers for pairs of parts surrounding gear symbols
 total = 0
 for y, part_map in symbol_grid.items():
     for x, part_numbers in part_map.items():
