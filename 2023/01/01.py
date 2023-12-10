c4b927c492a0f6a26af64dade02cdeaab236423e
James Addison <jay@jp-hosting.net>
2023-12-10 20:09:18 +0000

    2023-12-01: part two: solution code

--- 2023/01/01.py
+++ 2023/01/01.py
@@ -1,8 +1,28 @@
 content = open("01.txt").read().splitlines()
 
+NUMBERS = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
 total = 0
 for line in content:
-    numbers = [c for c in line if c.isdigit()]
+    numbers = []
+    while line:
+        if line[0].isdigit():
+            numbers.append(line[0])
+        else:
+            for word, number in NUMBERS.items():
+                if line.startswith(word):
+                    numbers.append(number)
+        line = line[1:]
     value = f"{numbers[0]}{numbers[-1]}"
     total += int(value)
 print(total)
