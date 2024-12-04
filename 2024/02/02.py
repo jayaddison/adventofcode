917740df85064ec65deb3801f3963af170b7b223
James Addison <jay@jp-hosting.net>
2024-12-04 13:08:41 +0000

    2024-12-02: part two: solution code
    Supersedes commit 52e926a22c6cab89ec66cee856aebbd4dd822174.
    
    With-credit-to: https://www.reddit.com/r/adventofcode/comments/1h4shdu/2024_day_2_part2_edge_case_finder/

--- 2024/02/02.py
+++ 2024/02/02.py
@@ -5,17 +5,25 @@ for line in sys.stdin.read().splitlines():
     levels = list(map(int, line.split()))
     rows.append(levels)
 
+def row_variants(row):
+    yield row
+    for i in range(len(row)):
+        yield [level for idx, level in enumerate(row) if idx != i]
+
 status = []
 for row in rows:
-    diffs, directions = set(), set()
-    prev, direction = None, None
-    for level in row:
-        if prev:
-            diffs.add(abs(prev - level))
-            directions.add(1 if level > prev else -1)
-        prev = level
+    for row_variant in row_variants(row):
+        diffs, directions = set(), set()
+        prev, direction = None, None
+        for level in row_variant:
+            if prev:
+                diffs.add(abs(prev - level))
+                directions.add(1 if level > prev else -1)
+            prev = level
 
-    safe = len(directions) == 1 and all(1 <= diff <= 3 for diff in diffs)
+        safe = len(directions) == 1 and all(1 <= diff <= 3 for diff in diffs)
+        if safe:
+            break
     status.append(safe)
 
 print(sum(status))
