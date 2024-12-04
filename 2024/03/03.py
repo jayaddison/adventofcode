f90f5a207cc84db3436c5e8a1fd3e6a0aef8e445
James Addison <jay@jp-hosting.net>
2024-12-04 13:29:53 +0000

    2024-12-03: part two: solution code

--- 2024/03/03.py
+++ 2024/03/03.py
@@ -2,5 +2,21 @@ import re
 import sys
 
 memory = sys.stdin.read()
-findings = re.finditer(r'mul\((\d{1,3}),(\d{1,3})\)', memory)
-print(sum(list(int.__mul__(*map(int, finding.groups())) for finding in findings)))
+multiplication_enabled = True
+multiplication_accumulator = 0
+
+findings = re.finditer(r"(do)\(\)|(don't)\(\)|(mul)\((\d{1,3}),(\d{1,3})\)", memory)
+for finding in findings:
+    filtered_groups = tuple(group for group in finding.groups() if group)
+    instruction = filtered_groups[0]
+    match instruction:
+        case "do":
+            multiplication_enabled = True
+        case "don't":
+            multiplication_enabled = False
+        case "mul" if multiplication_enabled:
+            arguments = map(int, filtered_groups[1:])
+            product = int.__mul__(*arguments)
+            multiplication_accumulator += product
+
+print(multiplication_accumulator)
